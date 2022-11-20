e9455ed820ae391f13c33ef85547896a06551ba4
BHyeonKim <rlaqhguse@gmail.com>
2022-11-21 02:14:27 +0900

    chore: added some rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,34 @@
 module.exports = {
   root: true,
-  plugins: ['react', 'react-hooks', 'prettier'],
+  env: {
+    node: true,
+    'react-native/react-native': true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', 'react-native', 'prettier'],
   extends: [
     '@react-native-community',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:react-native/all',
   ],
   rules: {
     semi: [2, 'always'],
     quotes: [2, 'single', {avoidEscape: true}],
     'prettier/prettier': 2,
+    'no-console': 1,
+    'import/named': 0,
+    'react/prop-types': 0,
   },
-  settings: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/extensions': ['.js', 'jsx'],
+  },
 };
