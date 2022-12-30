121f204004c939739f9fd604e6928ce60f599120
BHyeonKim <rlaqhguse@gmail.com>
2022-12-30 20:35:27 +0900

    chore: react-navigation, eslint 설정파일 설치

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: ['react', 'react-hooks', 'react-native', 'prettier'],
+  plugins: ['react', 'react-hooks', 'react-native', 'prettier', 'simple-import-sort', 'import'],
   extends: [
     '@react-native-community',
     'plugin:react/recommended',
@@ -18,11 +18,18 @@ module.exports = {
     'plugin:react-native/all',
   ],
   rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/named': 0,
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'react-native/no-unused-styles': 0,
+    'react-native/no-inline-styles': 0,
     semi: [2, 'always'],
     quotes: [2, 'single', {avoidEscape: true}],
     'prettier/prettier': 2,
     'no-console': 1,
-    'import/named': 0,
     'react/prop-types': 0,
   },
   settings: {
